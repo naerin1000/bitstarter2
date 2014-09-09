fbdc15ac470c1d21cef7c603c5a8770d1db9b8e5
startup.dongbaek@gmail.com <ubuntu@ip-172-31-44-159.us-west-2.compute.internal>
2014-09-09 06:15:22 +0000

    m

--- web.js
+++ web.js
@@ -1,19 +1,15 @@
-var http = require("http");
-var express = require("express");
-var morgan = require("morgan")
+var express = require('express');
+var morgan  = require('morgan');
 
 //var app = express.createServer(express.logger());
 var app = express();
+
+app.get('/', function(request, response) {
+  response.send("Hello World2222!");
+});
 app.use(morgan('dev'));
 
-app.use(function(request, response, next) {
- response.send("Hello World2222!");
-});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-
-http.createServer(app).listen(5000, function() {
-  console.log("Server Running at ...");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 }); 
